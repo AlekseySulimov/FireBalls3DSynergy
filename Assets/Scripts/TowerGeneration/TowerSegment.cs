2c272ba72c6292aab74b8e0c1237c180ac0017c0
IvanGriboedov <educateGriboedov@gmail.com>
2023-04-23 16:38:58 +0300

    CreatedTowerGenerator

--- Assets/Scripts/TowerGeneration/TowerSegment.cs
+++ Assets/Scripts/TowerGeneration/TowerSegment.cs
@@ -4,6 +4,9 @@ namespace TowerGeneration
 {
 	public class TowerSegment : MonoBehaviour
 	{
-		
+		public void SetMaterial(Material material)
+		{
+			GetComponent<MeshRenderer>().sharedMaterial = material;
+		}
 	}
 }

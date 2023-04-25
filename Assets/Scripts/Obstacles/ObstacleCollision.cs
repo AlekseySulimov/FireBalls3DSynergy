b3039fb9ad59af9d566ab0323b9255f39624796e
IvanGriboedov <educateGriboedov@gmail.com>
2023-04-25 10:01:09 +0300

    Create directions bouce

---
+++ Assets/Scripts/Obstacles/ObstacleCollision.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace Obstacles
+{
+	public class ObstacleCollision : MonoBehaviour
+	{
+		private void OnCollisionEnter(Collision other)
+		{
+			
+		}
+	}
+}

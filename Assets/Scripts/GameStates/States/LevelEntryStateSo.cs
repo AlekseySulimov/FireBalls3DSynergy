ae127a3dd4c8610e9120461b864cdf93f2513b5e
IvanGriboedov <educateGriboedov@gmail.com>
2023-04-27 17:22:54 +0300

    Created SceneLoading

--- Assets/Scripts/GameStates/States/LevelEntryStateSo.cs
+++ Assets/Scripts/GameStates/States/LevelEntryStateSo.cs
@@ -1,15 +1,17 @@
 ﻿using GameStates.Base;
+using SceneLoading;
 using UnityEngine;
 
 namespace GameStates.States
 {
 	public class LevelEntryStateSo : GameStateSo
 	{
-		[SerializeField] private string _levelName;
-		
+		[SerializeField] private Scene _scene;
+
+		private readonly IAsyncSceneLoading _sceneLoading = new UnitySceneLoading();
 		public override void Enter()
 		{
-			
+			_sceneLoading.LoadAsync(_scene);
 		}
 
 		public override void Exit() { }
---
+++ Assets/Scripts/SceneLoading/Scene.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine.SceneManagement;
+
+namespace SceneLoading
+{
+	public struct Scene
+	{
+		public string Name;
+		public LoadSceneMode LoadMode;
+	}
+}

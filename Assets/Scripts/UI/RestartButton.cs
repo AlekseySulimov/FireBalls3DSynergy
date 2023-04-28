b17455c1da7f4cce22a441fc875a2a4b6d8bdf9b
IvanGriboedov <educateGriboedov@gmail.com>
2023-04-28 19:12:02 +0300

    Created Level Losing

---
+++ Assets/Scripts/UI/RestartButton.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using GameStates.Base;
+using GameStates.States;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+	[RequireComponent(typeof(Button))]
+	public class RestartButton : MonoBehaviour
+	{
+		[SerializeField] private GameStateMachineSo _stateMachine;
+		
+		private void Start()
+		{
+			Button button= GetComponent<Button>();
+			button.onClick.AddListener(_stateMachine.Enter<LevelLoseState>);
+		}
+	}
+}

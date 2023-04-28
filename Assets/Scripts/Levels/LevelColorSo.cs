52d8caf8defbeb6a6bd55151cf30d0c1bc4a401d
IvanGriboedov <educateGriboedov@gmail.com>
2023-04-28 14:23:00 +0300

    Created Menu Levels

---
+++ Assets/Scripts/Levels/LevelColorSo.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Levels
+{
+	[CreateAssetMenu(fileName = "LevelColors", menuName = "ScriptableObject/Menu/Level/Colors")]
+	public class LevelColorSo : ScriptableObject
+	{
+		[SerializeField] private Color _passedLevels = Color.green;
+		[SerializeField] private Color _nextLevels = Color.white;
+		
+		public Color PassedLevels => _passedLevels;
+		public Color NextLevels => _nextLevels;
+		
+		
+		
+	}
+}

e722ec0ce3ba2e03a20b3019347d46bda996b474
IvanGriboedov <educateGriboedov@gmail.com>
2023-04-29 19:29:02 +0300

    Created LevelInitialization

---
+++ Assets/Scripts/DataPersistence/FilePathSo.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using UnityEngine;
+
+namespace DataPersistence
+{
+	[CreateAssetMenu(fileName = "FilePath", menuName = "ScriptableObject/Data/FilePath")]
+	public class FilePathSo : ScriptableObject
+	{
+		[SerializeField] private string _fileName;
+		
+		public string Value => Path.Combine(DirectoryPath, _fileName);
+
+		private string DirectoryPath =>
+			Application.isEditor
+				? Application.streamingAssetsPath
+				: Application.persistentDataPath;
+
+	}
+}
